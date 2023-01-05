e3e7cd603a67959284995cfb793a446f2d30351a
tamires <tamireshc@gmail.com>
2023-01-05 17:14:34 -0300

    fix align footer mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,13 @@ import heart from "../images/heart.svg";
 const Footer = () => {
   return (
     <footer className=" bg-stone-600 p-5 text-white font-lato text-sm ">
-      <div className="flex items-center justify-center">
-        <p className=" mr-1 md:mr-2">
-          © 2023 - Designed with React.js, TailwindCSS, Getform.io and
+      <div className="flex items-center justify-center md:flex-wrap ">
+        <p className=" mr-1 md:mr-2 text-end">
+          © 2023 - Designed with React.js, Getform.io, TailwindCSS and
         </p>
         <img src={heart} className="w-6 pulse" alt="imagem de coracao" />
-        <p className="ml-2 ">
-          by{" "}
+        <p className="ml-2 md:ml-1">
+          by
           <span className=" text-dark-rose ml-1">Tamires Sousa Batista.</span>
         </p>
       </div>
