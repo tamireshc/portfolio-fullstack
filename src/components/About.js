042b51688d01b4a927d1d3f06eb95b19d65370a7
tamires <tamireshc@gmail.com>
2024-05-30 15:14:49 -0300

    feat: update about

--- src/components/About.js
+++ src/components/About.js
@@ -13,12 +13,12 @@ const About = () => {
       <div className="flex justify-center">
         <img
           className="lg:mr-24 mb-10 lg:mb-2"
-          src={photoAbout}
+          src={ photoAbout }
           alt="foto da tamires sousa"
         />
       </div>
       <div className="max-w-3xl text-light-rose text-sm mb-4">
-        <img className="mb-8" src={about} alt="foto da tamires sousa" />
+        <img className="mb-8" src={ about } alt="foto da tamires sousa" />
         <p className="square-detail ml-4">
           I started my academic studies in chemistry. During this journey I
           developed research in Physical Chemistry with emphasis on
@@ -26,14 +26,12 @@ const About = () => {
         </p>
         <br></br>
         <p className="square-detail ml-4">
-          I continued to expand my knowledge and started
-          pursuing a career in technology. I took several courses about
-          programming for understand how it worked and if I really wanted this
-          career. During that first contact, I identified myself with the area,
-          and I looked for a way that I could become professional. Then I
-          discovered Trybe. A technology school that offers complete training
-          focused on the market.
+          After graduating in chemistry, I continued looking to expand my knowledge and started studying technology. I took several programming courses with the aim of better understanding how it worked and whether I really wanted this career.
         </p>
+        <p className="square-detail ml-4">
+          During this first contact, I identified with the area and looked for a way in which I could become a professional. I completed Full-Stack development training at Trybe and am currently studying the MBA in Software Engineering at USP.
+        </p>
+        <br></br>
       </div>
     </section>
   );
